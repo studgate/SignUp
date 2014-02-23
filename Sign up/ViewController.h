660e225663dfc222ab185c1446393488feca4ce8
Yuji Shimizu <yuji.shimizu@dena.com>
2014-02-22 17:03:49 -0800

    Initial Commit

---
+++ Sign up/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sign up
+//
+//  Created by Yuji Shimizu on 2/22/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Sign upTests/Sign_upTests.m
@@ -0,0 +1,34 @@
+//
+//  Sign_upTests.m
+//  Sign upTests
+//
+//  Created by Yuji Shimizu on 2/22/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sign_upTests : XCTestCase
+
+@end
+
+@implementation Sign_upTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
